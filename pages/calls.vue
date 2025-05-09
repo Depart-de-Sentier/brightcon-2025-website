01133121112297a7e07cfbf8ffb213a1988ca796
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2025-05-09 15:54:02 +0200

    feat: add calls and sponsoring pages
    signed-of: Tomás Navarrete Gutiérrez

---
+++ pages/calls.vue
@@ -0,0 +1,3 @@
+<template>
+  <Calls />
+</template>
---
+++ pages/sponsoring.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div>
+      <h2>Sponsoring</h2>
+      <p>
+        There are three packages for companies that wish to support the
+        conference and Brightway development. Please contact
+        <a href="mailto: cmutel@gmail.com">Chris Mutel</a> if you wish to be a
+        sponsor.
+      </p>
+
+      <h3>Palladium</h3>
+      <ul>
+        <li>Logo on website and registration area</li>
+        <li>Recognition on programme and press release</li>
+        <li>2 Free CONFERENCE registrations</li>
+        <li>Association to an award / contest winner</li>
+      </ul>
+      <p>
+        If your company has an interesting problem to solve with tools from the
+        Brightway ecosystem, participate by awarding a solution to a challenge
+        you can help to define: an issue your company is facing when using
+        Brightway, a feature you would like to see implemented, etc.
+      </p>
+
+      <h3>Gold</h3>
+      <ul>
+        <li>Logo on website and registration area</li>
+        <li>Recognition on programme and press release</li>
+        <li>1 free CONFERENCE registration</li>
+      </ul>
+      <p>
+        If you are actively using tools from the Brightway ecosystem or looking
+        forward to, join the conference and get trained on the basis or the
+        advanced functionalities of Brightway. Be trained on Brightway, and stay
+        for the conference to see how other people are using Brightway.
+      </p>
+
+      <h3>Nickel</h3>
+      <ul>
+        <li>Logo on website and registration area</li>
+        <li>3 minute pitch of company</li>
+      </ul>
+      <p>
+        The Brightcon audience is a great place to promote your activities:
+        sustainability practitioners, computational sustainability experts,
+        software developers engaged in sustainability might have a question, a
+        problem or an answer for you.
+      </p>
+    </div>
+    <p>
+      <b>
+        Please get in touch with the organizers to find out about the sponsoring
+        fees.
+      </b>
+    </p>
+  </div>
+  <template></template>
+</template>
